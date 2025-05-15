feb3be8391ac3a0e58df5d740cebafae9053040b
SMBNS <conceptualbaines@gmail.com>
2025-05-15 20:45:14 +0100

    Added global css reset & custom fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,4 +19,26 @@ export default defineConfig({
       }
     }),
     mdx()],
+    experimental: {
+        fonts: [{
+            provider: "local",
+            name: "Nohemi",
+            cssVariable: "--font-nohemi",
+            variants: [
+                {
+                    weight: 400,
+                    style: "normal",
+                    src: ["./src/fonts/Nohemi-Regular.woff2"]
+                },
+                {
+                    weight: 700,
+                    style: "normal",
+                    src: ["./src/fonts/Nohemi-SemiBold.woff2"]
+                }
+            ],
+            fallbacks: [
+              "Nohemi", "Helvetica", "sans-serif"
+            ]
+        }]
+    }
 });
